f0940507042243650d9ff140f7c832d0446f581f
Gustavo Alves <gustavo.alvesssss98@gmail.com>
2026-04-16 13:56:22 -0300

    Corrigindo login e validações

--- app/controllers/auth_controller.py
+++ app/controllers/auth_controller.py
@@ -22,16 +22,37 @@ def register():
     return jsonify({"msg": "Usuário criado"}), 201
 
 
-def login():
-    data = request.json
-    user = User.query.filter_by(username=data['username']).first()
 
-    if not user or not check_password_hash(user.password, data['password']):
-        return jsonify({"msg": "Credenciais inválidas"}), 401
+
+
+def login():
+    data = request.get_json()
+
+    if not data:
+        return jsonify({"msg": "JSON inválido"}), 400
+
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({"msg": "Campos obrigatórios faltando"}), 400
+
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        return jsonify({"msg": "Usuário não encontrado"}), 404
+
+    if not check_password_hash(user.password, password):
+        return jsonify({"msg": "Senha incorreta"}), 401
 
     token = create_access_token(identity={
         "id": user.id,
         "role": user.role
     })
 
-    return jsonify(access_token=token)
+    return jsonify(access_token=token), 200
+
+
+
+
+
