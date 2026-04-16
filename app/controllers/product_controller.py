04a4b3fa4a6220d10df6f4d64765dd82d5e98c0d
Gustavo Alves <gustavo.alvesssss98@gmail.com>
2026-04-16 14:45:33 -0300

    corrigindo JWT no product controller

--- app/controllers/product_controller.py
+++ app/controllers/product_controller.py
@@ -1,27 +1,30 @@
 from flask import request, jsonify
 from app.models.product import Product
 from app import db
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_identity, get_jwt
+
 
 @jwt_required()
 def create_product():
-    user = get_jwt_identity()
+    user_id = int(get_jwt_identity())
+    claims = get_jwt()
+    role = claims.get("role")
 
-    if user['role'] != 'admin':
+    if role != 'administrador':
         return {"msg": "Apenas admin pode criar"}, 403
 
-    data = request.json
+    data = request.get_json()
 
     product = Product(
         name=data['name'],
         price=data['price'],
-        user_id=user['id']
+        user_id=user_id
     )
 
     db.session.add(product)
     db.session.commit()
 
-    return {"msg": "Produto criado"}
+    return {"msg": "Produto criado"}, 201
 
 
 @jwt_required()
@@ -40,16 +43,19 @@ def list_products():
 
 @jwt_required()
 def update_product(id):
-    user = get_jwt_identity()
+    user_id = int(get_jwt_identity())
+    claims = get_jwt()
+    role = claims.get("role")
+
     product = Product.query.get_or_404(id)
 
-    if user['role'] != 'admin' and product.user_id != user['id']:
+    if role != 'administrador' and product.user_id != user_id:
         return {"msg": "Sem permissão"}, 403
 
-    data = request.json
+    data = request.get_json()
 
-    product.name = data['name']
-    product.price = data['price']
+    product.name = data.get('name', product.name)
+    product.price = data.get('price', product.price)
 
     db.session.commit()
 
@@ -58,10 +64,12 @@ def update_product(id):
 
 @jwt_required()
 def delete_product(id):
-    user = get_jwt_identity()
+    claims = get_jwt()
+    role = claims.get("role")
+
     product = Product.query.get_or_404(id)
 
-    if user['role'] != 'admin':
+    if role != 'administrador':
         return {"msg": "Apenas admin pode deletar"}, 403
 
     db.session.delete(product)
